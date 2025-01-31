35de03008acbdb443721b5b5a3414081f01dd1e4
RayanKotobRubika <r.elkotob@rubika-edu.com>
2025-01-31 10:53:38 +0100

    OMG CA MARCHE

--- Assets/_Scripts/Lanes/LaneArrival.cs
+++ Assets/_Scripts/Lanes/LaneArrival.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class LaneArrival : MonoBehaviour
 {
     [SerializeField] private WaitingQueues[] _sliders;
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.TryGetComponent(out Passenger passenger)) return;
-
+    
         foreach (WaitingQueues slider in _sliders)
         {
             if (passenger.Color != slider.Color) continue;
